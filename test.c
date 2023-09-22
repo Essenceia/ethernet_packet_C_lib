b4e14c1bb0fc4f7a4ceb8f611ca4409ba4f98c20
Julia Desmazes <julia.desmazes@gmail.com>
2023-09-21 21:19:19 -0700

    TEST: Add simple crc test

--- test.c
+++ test.c
@@ -1,10 +1,19 @@
 #include "eth_packet_s.h"
 #include <stdio.h>
+#include <assert.h>
 
 int main(){
+	uint8_t t[1] = {0};
+
 	eth_packet_s *pkt;	
 
 	printf("Running test\n");
+	
+	/* test crc */
+	uint32_t crc = calculate_crc(t, 1);
+	assert(crc == 0x4E08BFB4);
+
+	printf("PASS: data { 0 } crc result : %08x\n", crc);
 
 	return 0;
 }
